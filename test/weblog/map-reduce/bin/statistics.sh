f1e8007af28164b64a3062362e5f286eff0ac3b1
bruce@cloudera.com <bruce@cloudera.com>
2015-02-21 04:57:15 -0800

    Removing data files

--- test/weblog/map-reduce/bin/statistics.sh
+++ test/weblog/map-reduce/bin/statistics.sh
@@ -1,3 +1,3 @@
 echo "Compute SARAH statistics on weblog"
 hadoop fs -rm -r weblog.sarah
-sarah statistics -libjars ../lib/sarahtest.jar -conf conf/statistics.xml weblog 
+sarah statistics -libjars $SARAH_TEST/weblog/map-reduce/lib/sarahtest.jar -conf $SARAH_TEST/weblog/map-reduce/bin/conf/statistics.xml weblog 
